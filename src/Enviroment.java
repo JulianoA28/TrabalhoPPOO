2d717f3258961d4984a17d46d171873f2a95cdd9
Amanda Leal <40030423+Amandaleal@users.noreply.github.com>
2021-03-03 23:44:13 -0300

    Adicionando javadoc

--- src/Enviroment.java
+++ src/Enviroment.java
@@ -1,12 +1,22 @@
+
+/**
+ * Classe responsável por definir o comportamento das variaveis do ambiente, 
+ * e como o mesmo interfere no comportamento dos animais facilitando ou dificultado.
+ * 
+ * @author Amanda Cassiano Leal
+ */
 import java.util.List;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.HashMap;
 
 public class Enviroment extends Actor {
-
+	/*
+	 * O bonus de procriacao que sera incrmentado na probabilidade de procriacao do
+	 * animal que se movimentar pelo ambiente no campo atual.
+	 */
 	private static final HashMap<String, Double> BREEDING_BONUS = new HashMap<String, Double>();
-
+	// Mapeamento dos atributos atraves de um par chave, valor
 	static {
 		BREEDING_BONUS.put("Crop", 3.0);
 		BREEDING_BONUS.put("Hole", 1.5);
@@ -15,16 +25,25 @@ public class Enviroment extends Actor {
 
 	private Field field;
 
+	// status do ambiente (true = ativado)
 	private boolean active;
 
+	/**
+	 * @param active true ou false para ativar a acao do ambiente
+	 **/
 	public void setActive(boolean active) {
 		this.active = active;
 	}
 
+	/**
+	 * 
+	 * @return o valor do bonus que aquele ambiente proporciona
+	 */
 	public double getBonus() {
 		return BREEDING_BONUS.get(this.getClass().getName());
 	}
 
+	// construtor
 	public Enviroment() {
 
 	}
